f2c0c5e5e67da10875a26a54c2cbc9f79a6731e3
Chris Hill <cnh@mit.edu>
2025-03-10 08:17:19 -0400

    Update check_result_command.cmd

--- check_result_command.cmd
+++ check_result_command.cmd
@@ -1,1 +1,4 @@
+# One hand written line for testing whether generated code is working
+# For test a.txt and b.txt it tests the number of entries for each document in 
+# assu=ignments.txt. Each document should appear twice only.
 cat assignments.txt |  tr ',' ' ' | tr ' ' '\n' | sort | uniq -c | grep ' b[0-9]'
